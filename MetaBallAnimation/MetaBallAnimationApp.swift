2d29468ec09d5886dd4ef532021517a3afbf868c
Ahmed Nagy <ahmednaagy97@gmail.com>
2022-09-26 10:10:51 +0200

    Initial Commit

---
+++ MetaBallAnimation/MetaBallAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaBallAnimationApp.swift
+//  MetaBallAnimation
+//
+//  Created by Ahmed Nagy on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MetaBallAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
